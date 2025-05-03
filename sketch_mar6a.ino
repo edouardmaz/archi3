949c7e64572c3cffea0fbb0cbf0c6f0738b1c205
edouardmaz <edouard.mazars@outlook.fr>
2025-05-03 10:31:02 +0200

    finition

--- sketch_mar6a.ino
+++ sketch_mar6a.ino
@@ -3,10 +3,12 @@ int bp1 = 2;
 int bp2 = 3;
 int bp3=18;
 int bp4=19;
+//const int buzz=14;
 int muco;
 
 #include <DFRobot_RGBMatrix.h> // Hardware-specific library
 #include <Wire.h>
+//#include "MeAuriga.h"
 #define OE    9
 #define LAT   10
 #define CLK   11
@@ -25,6 +27,8 @@ int play=1;
 int lastScore = -1; // Pour détecter les changements de score
 int level = 1;
 DFRobot_RGBMatrix matrix(A, B, C, D, E, CLK, LAT, OE, false, WIDTH, HIGH);
+bool start = false; // bool pour le menu pour commencer le jeu
+//MeBuzzer buzzer;
 
 // Définition des couleurs
 uint16_t red = matrix.Color333(7, 0, 0);
@@ -33,17 +37,17 @@ uint16_t green = matrix.Color333(0, 7, 0);
 uint16_t yellow = matrix.Color333(7, 7, 0);
 uint16_t white = matrix.Color333(7, 7, 7);
 uint16_t black = matrix.Color333(0, 0, 0);
-uint16_t scoreColor = matrix.Color333(0, 7, 0); // Vert vif
 uint16_t bgColor = matrix.Color333(1, 1, 1);    // Gris pour le fond
 
 volatile long int temps1 = 0;
 uint16_t game[10][20];
-
-int pile1[] = {33, 3, 35, 5};
-int pile2[] = {36, 3, 38, 5};
-uint16_t c1 = red;
-uint16_t c2 = blue;
-bool fa = true;
+int gamepile[10][20][2];
+int pile1[4];
+int pile2[4];
+uint16_t c1;
+uint16_t c2;
+bool fa = false;
+bool move=false;
 
 void setup() {
   randomSeed(analogRead(0));
@@ -52,61 +56,48 @@ void setup() {
   pinMode(bp1, INPUT_PULLUP);
   pinMode(bp2, INPUT_PULLUP);
   pinMode(bp3,INPUT_PULLUP);
+  pinMode(bp4,INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(bp1), stepleft, FALLING);
   attachInterrupt(digitalPinToInterrupt(bp2), stepright, FALLING);
   attachInterrupt(digitalPinToInterrupt(bp3), spin, FALLING);
+  attachInterrupt(digitalPinToInterrupt(bp4), commencer, FALLING);
   matrix.begin();
   
   // Initialisation du tableau de jeu
   for (int i = 0; i < 10; i++) {
     for (int j = 0; j < 20; j++) {
       game[i][j] = black;
+      gamepile[i][j][0]=0;
+      gamepile[i][j][1]=0;
     }
   }
-  // Initialisation de l'affichage
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
-  drawBorders();
-  drawDivider();
-  displayScore();
-  displayLevel();
-  
-  matrix.setTextColor(matrix.Color333(4, 0, 4));
-  
-  delay(500);
-  game[1][19] = red;
-  drawVirus(1,19);
-  game[2][19] = red;
-  drawVirus(2,19);
-  game[3][19] = red;
-  drawVirus(3,19);
-
-  game[4][14] = red;
-  drawVirus(4,14);
-  game[4][15] = red;
-  drawVirus(4,15);
-  game[4][16] = red;
-  drawVirus(4,16);
-
-  //generateViruses(10); //30
 }
 
 void envoi(int n) {
-  Serial.println(n);
-  //Serial2.print(n);
+  Serial2.print(n);
 }
 
 void drawBorders() {
+
+  // Game Borders
   matrix.drawLine(0, 0, 63, 0, white);
   matrix.drawLine(0, 0, 0, 63, white);
   matrix.drawLine(0, 63, 63, 63, white);
   matrix.drawLine(63, 0, 63, 63, white);
+
+  // Border underneath Score
+  matrix.drawLine(1, 22, 30, 22, white);
+  matrix.drawLine(1, 27, 30, 27, white);
+
+  // Border underneath Level
+  matrix.drawLine(1, 45, 30, 45, white);
+  matrix.drawLine(1, 46, 30, 46, white);
 }
 
 void drawDivider() {
-  int border = 31;
   for (int i = 0; i < 64; i++) {
-    matrix.drawPixel(border, i, white); 
-    matrix.drawPixel(border + 1, i, white); 
+    matrix.drawPixel(31, i, white); 
+    matrix.drawPixel(31 + 1, i, white); 
   }
 }
 
@@ -115,16 +106,16 @@ void displayScore() {
   matrix.setTextSize(1);
   matrix.setCursor(1, 1);
   matrix.setTextColor(white);
-  matrix.println("Score:");
+  matrix.println("Score");
   matrix.setCursor(1, 10);
-  matrix.setTextColor(scoreColor);
+  matrix.setTextColor(green);
   matrix.println(score);
 }
 
 void displayLevel() {
   matrix.fillRect(1, 20, 30, 12, bgColor);
   matrix.setTextSize(1);
-  matrix.setCursor(1, 20);
+  matrix.setCursor(2, 27);
   matrix.setTextColor(white);
   matrix.println("Level");
   matrix.setCursor(1, 28);
@@ -138,15 +129,15 @@ void updateScoreDisplay() {
     matrix.fillRect(1, 10, 30, 8, bgColor);
     matrix.setTextSize(1);
     matrix.setCursor(1, 10);
-    matrix.setTextColor(scoreColor);
+    matrix.setTextColor(green);
     matrix.println(score);
-    delay(50);
+    //delay(50);
     
     // Retour à l'affichage normal
     matrix.fillRect(1, 10, 30, 8, bgColor);
     matrix.setTextSize(1);
     matrix.setCursor(1, 10);
-    matrix.setTextColor(scoreColor);
+    matrix.setTextColor(green);
     matrix.println(score);
     
     lastScore = score;
@@ -205,64 +196,57 @@ void drawPile(int pile[], uint16_t c) {
   }
 }
 
+void commencer(){
+  start=true;
+}
+
 void stepleft() {
   long int temps = micros();
-  drawPile(pile1, black);
-  drawPile(pile2, black);
   int x1 = pile1[0];
   int x2 = pile2[0];
   int y1 = pile1[1];
   int y2 = pile2[1];
-  
-  if (x1 > 33 && x2 > 33 && game[(x1 - 33) / 3 - 1][(y1 - 3) / 3] == black && 
-      game[(x2 - 33) / 3 - 1][(y2 - 3) / 3] == black && fa) {
-    if (temps - temps1 > 100000) {
-      pile1[0] = x1 - 3;
-      pile1[2] = x1 - 1;
-      pile2[0] = x2 - 3;
-      pile2[2] = x2 - 1;
-    }
-  }
-  
-  fa = hit(pile1, c1, fa);
-  fa = hit(pile2, c2, fa);
-  fa = hit(pile1, c1, fa);
-  drawPile(pile1, c1);
-  drawPile(pile2, c2);
-  temps1 = temps;
+  if (temps - temps1 > 100000 && x1 > 33 && x2 > 33 && game[(x1 - 33) / 3 - 1][(y1 - 3) / 3] == black && game[(x2 - 33) / 3 - 1][(y2 - 3) / 3] == black && fa && move) {
+    drawPile(pile1, black);
+    drawPile(pile2, black);
+    pile1[0] = x1 - 3;
+    pile1[2] = x1 - 1;
+    pile2[0] = x2 - 3;
+    pile2[2] = x2 - 1;
+    fa = hit(pile1,pile2, c1, fa);
+    fa = hit(pile2,pile1, c2, fa);
+    fa = hit(pile1,pile2, c1, fa);
+    drawPile(pile1, c1);
+    drawPile(pile2, c2);
+    temps1 = temps;
+  }  
 }
 
 void stepright() {
   long int temps = micros();
-  drawPile(pile1, black);
-  drawPile(pile2, black);
   int x1 = pile1[2];
   int x2 = pile2[2];
   int y1 = pile1[1];
   int y2 = pile2[1];
-  
-  if (x1 < 62 && x2 < 62 && game[(x1 - 33) / 3 + 1][(y1 - 3) / 3] == black && 
-      game[(x2 - 33) / 3 + 1][(y2 - 3) / 3] == black && fa) {
-    if (temps - temps1 > 100000) {
-      pile1[0] = x1 + 1;
-      pile1[2] = x1 + 3;
-      pile2[0] = x2 + 1;
-      pile2[2] = x2 + 3;
-    }
+  if (temps - temps1 > 100000 && x1 < 62 && x2 < 62 && game[(x1 - 33) / 3 + 1][(y1 - 3) / 3] == black && game[(x2 - 33) / 3 + 1][(y2 - 3) / 3] == black && fa && move) {
+    drawPile(pile1, black);
+    drawPile(pile2, black);
+    pile1[0] = x1 + 1;
+    pile1[2] = x1 + 3;
+    pile2[0] = x2 + 1;
+    pile2[2] = x2 + 3;
+    fa = hit(pile1,pile2, c1, fa);
+    fa = hit(pile2,pile1, c2, fa);
+    fa = hit(pile1,pile2, c1, fa);
+    drawPile(pile1, c1);
+    drawPile(pile2, c2);
+    temps1 = temps;
   }
-  
-  fa = hit(pile1, c1, fa);
-  fa = hit(pile2, c2, fa);
-  fa = hit(pile1, c1, fa);
-  drawPile(pile1, c1);
-  drawPile(pile2, c2);
-  temps1 = temps;
 }
-
 
 void spin(){
   long int temps=micros();
-  if (temps-temps1>100000){
+  if (temps-temps1>100000 and fa){
     drawPile(pile1,black);
     drawPile(pile2,black);
     int x1=pile1[0]/3-11;
@@ -282,7 +266,7 @@ void spin(){
         pile2[1]=pile1[1]-3;
         pile2[3]=pile1[3]-3;
       }
-    }else if (x1>0){
+    }else if (x1==x2 && x1>0){ //x1==x2 nécessaire pour eviter bug du no pile land
       if(y2>y1 && game[x1-1][y2]==black){
         pile2[0]=pile2[0]-3;
         pile2[2]=pile2[2]-3;
@@ -295,27 +279,68 @@ void spin(){
         pile2[3]=pile1[3];
       }
     }
-    fa = hit(pile1, c1, fa);
-    fa = hit(pile2, c2, fa);
-    fa = hit(pile1, c1, fa);
+    fa = hit(pile1,pile2, c1, fa);
+    fa = hit(pile2,pile1, c2, fa);
+    fa = hit(pile1,pile2, c1, fa);
     drawPile(pile1,c1);
     drawPile(pile2,c2);
     temps1=temps;
   }
 }
 
+void tofall(){
+  long int temps=micros();
+  if (temps-temps1>100000 && move){
+    drawPile(pile1,black);
+    drawPile(pile2,black);
+    while (fa){
+      pile1[1]=pile1[1]+3;
+      pile2[1]=pile2[1]+3;
+      pile1[3]=pile1[3]+3;
+      pile2[3]=pile2[3]+3;
+      fa = hit(pile1,pile2, c1, fa);
+      fa = hit(pile2,pile1, c2, fa);
+      fa = hit(pile1,pile2, c1, fa);
+    }
+  drawPile(pile1,c1);
+  drawPile(pile2,c2);
+  temps1=temps;
+  }
+}
+
+void topile(int pile[],uint16_t c){
+  int x=pile[0]/3-11;
+  int y=pile[1]/3-1;
+  //Serial.print(pile[1]);
+  game[x][y]=black;
+  bool tp=true;
+  drawPile(pile,black);
+  while (tp){
+    if (game[pile[0]/3-11][(pile[1]/3-1)+1]!=black || pile[1]==63){
+      tp=false;
+    }else{
+    pile[1]=pile[1]+3;
+    pile[3]=pile[3]+3;
+    }
+  }
+  game[x][y]=c;
+  drawPile(pile,c);
+}
+
 int fall(int py) {
   return py + 3;
 }
 
-bool hit(int pile[], uint16_t c, bool fa) {
-  int x1=pile1[0]/3-11;
-  int y1=pile1[1]/3-1;
-  int x2=pile[2]/3-11;
-  int y2=pile[3]/3-1;
+bool hit(int pilea[],int pileb[], uint16_t c, bool fa) {
+  int x1=pilea[0]/3-11;
+  int y1=pilea[1]/3-1;
+  int x2=pilea[2]/3-11;
+  int y2=pilea[3]/3-1;
   
   if (y2+1>19 || game[x2][y2+1]!=black || fa==false){
     game[x2][y2]=c;
+    gamepile[x2][y2][0]=pileb[0];
+    gamepile[x2][y2][1]=pileb[1];
     score += 10; // Augmentation du score quand un bloc se pose
     updateScoreDisplay();
     fa = false;
@@ -323,46 +348,67 @@ bool hit(int pile[], uint16_t c, bool fa) {
   return fa;
 }
 
-void cleanBoard(int x,int y,String dir,int nb){
-  if(dir=="r"){
+void cleanBoard(int x,int y,char dir,int nb){
+  int pile[4];
+  //Serial.print("do you?");
+  if(dir=='r'){
     for(int i=0;i<nb;i++){
       game[x+i][y]=black;
+      if (gamepile[x+i][y][0]!=0){
+        pile[0]=gamepile[x+i][y][0];;
+        pile[1]=gamepile[x+i][y][1];;
+        pile[2]=pile[0]+3;;
+        pile[3]=pile[1]+3;;
+        uint16_t pc=game[pile[0]/3-11][pile1[1]/3-1];
+        //topile(pile,pc);
+      }
       int pileb[]={(x+i)*3+33,y*3+3,(x+i)*3+35,y*3+5};
       drawPile(pileb,black);
     }
   }
   else{
     for(int i=0;i<nb;i++){
-      game[x][y+i]=black;
-      int pileb[]={x*3+33,(y+i)*3+3,x*3+35,(y+i)*3+5};
+      game[x][y-i]=black;
+      if (gamepile[x][y-i][0]!=0){
+        pile[0]=gamepile[x][y-i][0];;
+        pile[1]=gamepile[x][y-i][1];;
+        Serial.println(pile[0]);
+        Serial.print(pile[1]);
+        pile[2]=pile[0]+2;;
+        pile[3]=pile[1]+2;;
+        uint16_t pc=game[pile[0]/3-11][pile1[1]/3-1];
+        //topile(pile,pc);
+      }
+      int pileb[]={x*3+33,(y-i)*3+3,x*3+35,(y-i)*3+5};
       drawPile(pileb,black);
     }
   }
-  //envoi(nb-3);
+  envoi(nb-3);
 }
 
-int heal(int x,int y,uint16_t c,String dir,int nb){
+int heal(int x,int y,uint16_t c,char dir,int nb){
+  //Serial.println(dir);
   int r=0;
   int l=0;
   int d=0;
   int u=0;
-  if ((dir=="n"|| dir=="r") && game[x+1][y]==c){
+  if ((dir=='n'|| dir=='r') && game[x+1][y]==c){
     int h1=nb;
-    h1=heal(x+1,y,c,"r",h1+1);
+    h1=heal(x+1,y,c,'r',h1+1);
     if(h1>3 && nb==1){
       r=h1;
-      cleanBoard(x,y,"r",r);
+      cleanBoard(x,y,'r',r);
     }
     else if (h1!=0){
       return h1;
     }
   }
-  if ((dir=="n"|| dir=="d") && game[x][y+1]==c){
+  if ((dir=='n'|| dir=='u') && game[x][y-1]==c){
     int h3=nb;
-    h3=heal(x,y+1,c,"d",h3+1);
+    h3=heal(x,y-1,c,'u',h3+1);
     if(h3>3 && nb==1){
-      d=h3;
-      cleanBoard(x,y,"d",d);
+      u=h3;
+      cleanBoard(x,y,'u',u);
     }
     else if (h3!=0){
       return h3;
@@ -385,13 +431,13 @@ int checkborderx(int x,int y,uint16_t c){
 }
 int checkbordery(int x,int y,uint16_t c){
   int nuy=0;
-  while (game[x][y-nuy-1]==c){
+  while (game[x][y+nuy+1]==c){
     nuy++;
   }
   return nuy;
 }
 
-void treugame(){  // ajoute l'affichage du modèle
+/*void treugame(){  // ajoute l'affichage du modèle
   for(int i=0;i<20;i++){
     for (int j=0;j<10;j++){
       Serial.print("[");
@@ -404,17 +450,59 @@ void treugame(){  // ajoute l'affichage du modèle
     }
     Serial.println();
   }
-}
+}*/
 
 void loop() {
+  while (!start) {
+    // Game start!
+    // Main Menu
+    // Borders
+    matrix.drawLine(0, 0, 63, 0, white);
+    matrix.drawLine(0, 0, 0, 63, white);
+    matrix.drawLine(0, 63, 63, 63, white);
+    matrix.drawLine(63, 0, 63, 63, white);
+
+    // Dr Mario Text
+    matrix.setTextSize(1);
+    matrix.setCursor(10,10);
+    matrix.setTextColor(yellow);
+    matrix.println("Dr.Mario");
+
+    // Start button
+    matrix.setTextSize(1);
+    matrix.setCursor(17,40);
+    matrix.setTextColor(white);
+    matrix.println("Start");
+    if (start) {
+       pile1[0] = 33; pile1[1] = 3; pile1[2] = 35; pile1[3] = 5;
+      pile2[0] = 36; pile2[1] = 3; pile2[2] = 38; pile2[3] = 5;
+      c1 = randomColor();
+      c2 = randomColor();
+      drawPile(pile1, c1);
+      drawPile(pile2, c2);
+      attachInterrupt(digitalPinToInterrupt(bp4), tofall, FALLING);
+      //buzzer.tone(500,10);
+      // Initialisation de l'affichage
+      matrix.fillScreen(matrix.Color333(0, 0, 0));
+      drawBorders();
+      drawDivider();
+      displayScore();
+      displayLevel();
+      
+      matrix.setTextColor(matrix.Color333(4, 0, 4));
+      delay(500);
+
+      generateViruses(10);
+    }
+  }
   if (Serial2.available() > 0) {
-    muco = Serial2.read();
-    Serial.println(muco);
-    //generateViruses(muco);
+    muco = Serial2.read()-48;
+    generateViruses(muco);
   }
   int x=pile1[0]/3-11;
   int y=pile1[1]/3-1;
   if (fa) {
+    move=false;
     drawPile(pile1, black);
     drawPile(pile2, black);
     
@@ -426,22 +514,27 @@ void loop() {
     drawPile(pile1, c1);
     drawPile(pile2, c2);
     
-    fa = hit(pile1, c1, fa);
-    fa = hit(pile2, c2, fa);
-    fa = hit(pile1, c1, fa); //<- nécéssaire pour heal()
+    fa = hit(pile1,pile2, c1, fa);
+    fa = hit(pile2,pile1, c2, fa);
+    fa = hit(pile1,pile2, c1, fa); //<- nécéssaire pour heal()
+    move=true;
     
   } else if (play==1) {
     int nux=x-checkborderx(x,y,c1);
-    int nuy=y-checkbordery(x,y,c1);
-    heal(nux, y, c1, "n", 1);
-    heal(x, nuy, c1, "n", 1);
+    int nuy=y+checkbordery(x,y,c1);
+    if (x!=nux){
+      heal(nux, y, c1, 'n', 1);
+    }
+    heal(x, nuy, c1, 'n', 1);
     
     x = pile2[0] / 3 - 11;
     y = pile2[1] / 3 - 1;
     nux=x-checkborderx(x,y,c2);
-    nuy=y-checkbordery(x,y,c2);
-    heal(nux, y, c2, "n", 1);
-    heal(x, nuy, c2, "n", 1);
+    nuy=y+checkbordery(x,y,c2);
+    if (x!=nux){
+      heal(nux, y, c2, 'n', 1);
+    }
+    heal(x, nuy, c2, 'n', 1);
 
     
     // Réinitialiser les piles pour la chute suivante
@@ -461,9 +554,9 @@ void loop() {
       fa = true;
     }
   }
-  
+  //treugame();
   updateScoreDisplay();
-  delay(1000 - (level * 50)); // Le jeu s'accélère avec le niveau
+  delay(2000 - (level * 50)); // Le jeu s'accélère avec le niveau
 }
 
 // Les fonctions heal(), cleanBoard() et comboEffect() restent identiques à votre version originale
